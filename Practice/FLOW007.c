743658c1e6fdadf6986f20f39f0ad65c9e46fe3f
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-08-25 23:04:34 +0530

    Reverse the Number

---
+++ Practice/FLOW007.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	long int n,j,k;
+	int i,t,a[1000]={0};
+	scanf("%d",&t);
+	for(i=0;i<t;i++)
+	{
+		scanf("%ld",&n);
+		j=n;
+		while(j!=0)
+		{
+			k=j%10;
+			a[i]=10*a[i]+k;
+			j=j/10;
+		}
+	}
+	for(i=0;i<t;i++)
+		printf("%d\n",a[i]);
+	return 0;
+}
